0fd9e5298ab58d537d1293cdcf0f31a8ca6ecabd
Filip Lubka <filip.lubka2007@gmail.com>
2024-09-30 21:03:26 +0200

    started learning php

---
+++ pwi/php_intro/index.php
@@ -0,0 +1,117 @@
+<?php
+
+
+#ZAD1
+// $x = 2;
+// if($x > 0)
+// {
+//     echo"dodatnia";
+// }
+// elseif ($x < 0)
+// {
+//     echo"ujemna";
+// }
+// else{
+//     "zero";
+// }
+
+
+#ZAD2
+// function glosowanie($wiek){
+//     if ($wiek >= 18){
+//         echo"moze glosowac";
+//     }
+//     else{
+//         echo"nie moze";
+//     }
+// }
+// glosowanie(1);
+
+
+#ZAD3
+// $i=0;
+// $k=1;
+// if($k > $i){
+//     echo"k = ".$k;
+// }
+// else{
+//     echo "i = ".$i;
+// }
+
+
+##ZAD4
+// $imiona = array("JOHN", "john", "john");
+// foreach($imiona as $key => $value) {
+//     echo "". $key .") ". $value .", ";
+// }
+
+
+##ZAD5
+// $hashmap = array(
+//     "imie" => array("Jan"),
+//     "nazwisko" => array("pedal"),
+// );
+// foreach($hashmap as $key => $value){
+//     echo $key.") ".$value [0]."<br>";
+// }
+
+
+##ZAD6
+// $numbers = array(0,1,102,4,90);
+// sort( $numbers );
+// echo $numbers[count($numbers) - 1];
+
+
+##ZAD7
+// $i = 0;
+// do{
+//     echo ++$i;
+// }
+// while($i < 10);
+
+
+##ZAD8
+// $months = array(
+//     "January" => 31,
+//     "February" => 28,
+//     "March" => 31,
+//     "April" => 30,
+//     "May" => 31,
+//     "June" => 30,
+//     "July" => 31,
+//     "August" => 31,
+//     "September" => 30,
+//     "October" => 31,
+//     "November" => 30,
+//     "December" =>31,
+// );
+// $wynik = 0;
+// foreach ($months as $month => $value) 
+// {
+//     echo $month." --> ".$value."<br>";
+//     $wynik += $value;
+// }
+// echo "".$wynik."";
+
+
+##ZAD9
+for ($i = 0, $k = $i; $i < 50; $i++, $k+=$i){} 
+echo $k;
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+?>
